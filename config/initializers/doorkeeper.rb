6e6f72a54785435a042ae408341eee083411fc5b
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2025-11-20 17:48:14 +0630

    fix: update doorkeeper configuration and gem dependencies

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -11,15 +11,7 @@ Doorkeeper.configure do
 
   # Disable Resource Owner Password Credentials Grant Flow
   resource_owner_from_credentials do |_routes|
-    user   = User.authenticate_with_ldap(email: request.params[:username], password: request.params[:password]) if Devise.ldap_authentication
-    user ||= User.authenticate_with_pam(email: request.params[:username], password: request.params[:password]) if Devise.pam_authentication
-
-    if user.nil?
-      user = User.find_by(email: request.params[:username])
-      user = nil unless user&.valid_password?(request.params[:password])
-    end
-
-    user unless user&.otp_required_for_login?
+    nil
   end
 
   # Doorkeeper provides some administrative interfaces for managing OAuth
@@ -173,7 +165,7 @@ Doorkeeper.configure do
   #   http://tools.ietf.org/html/rfc6819#section-4.4.3
   #
 
-  grant_flows %w(authorization_code password client_credentials)
+  grant_flows %w(authorization_code client_credentials)
 
   # Under some circumstances you might want to have applications auto-approved,
   # so that the user skips the authorization step.
